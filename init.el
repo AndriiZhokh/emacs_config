fababac61f8023a62207b59fd4717c1b3726cf75
Andrii Zhokh <andriizhokh@gmail.com>
2022-05-13 10:15:57 +0300

    org mode stup

--- init.el
+++ init.el
@@ -1,3 +1,13 @@
+;;  ______              __                     ________   __              __      __         
+;; /\  _  \            /\ \         __  __    /\_____  \ /\ \            /\ \    /\ \        
+;; \ \ \L\ \    ___    \_\ \  _ __ /\_\/\_\   \/____//'/'\ \ \___     ___\ \ \/'\\ \ \___    
+;;  \ \  __ \ /' _ `\  /'_` \/\`'__\/\ \/\ \       //'/'  \ \  _ `\  / __`\ \ , < \ \  _ `\  
+;;   \ \ \/\ \/\ \/\ \/\ \L\ \ \ \/ \ \ \ \ \     //'/'___ \ \ \ \ \/\ \L\ \ \ \\`\\ \ \ \ \ 
+;;    \ \_\ \_\ \_\ \_\ \___,_\ \_\  \ \_\ \_\    /\_______\\ \_\ \_\ \____/\ \_\ \_\ \_\ \_\
+;;     \/_/\/_/\/_/\/_/\/__,_ /\/_/   \/_/\/_/    \/_______/ \/_/\/_/\/___/  \/_/\/_/\/_/\/_/
+                       
+
+;; Basic UI configuration ----------------------------------------------------
 (setq inhibit-startup-message t)
 
 (scroll-bar-mode -1)  ; Disable visible scrollbar
@@ -221,7 +231,63 @@
 ;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
 ;; (use-package forge)
 
-(use-package org)
+;; Org Mode Configuration ---------------------------------------------------------------
+
+(defun efs/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(defun efs/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Consolas" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . efs/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+	org-hide-emphasis-markers t)
+  (efs/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun efs/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . efs/org-mode-visual-fill))
+
+
+
+
 
 ;; Some code that was generated automaticaly
 (custom-set-variables
@@ -232,7 +298,7 @@
  '(custom-safe-themes
    '("6c531d6c3dbc344045af7829a3a20a09929e6c41d7a7278963f7d3215139f6a7" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" default))
  '(package-selected-packages
-   '(forge evil-magit magit counsel-projectile projectile hydra evil-collection evil general helpful doom-themes counsel swiper ivy-rich which-key rainbow-delimiters doom-modeline ivy use-package command-log-mode)))
+   '(visual-fill-column org-bullets forge evil-magit magit counsel-projectile projectile hydra evil-collection evil general helpful doom-themes counsel swiper ivy-rich which-key rainbow-delimiters doom-modeline ivy use-package command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
