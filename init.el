5eeb60d74cbd4185023ceafbc66a7143122666a2
Andrii Zhokh <andriizhokh@gmail.com>
2022-07-19 17:00:23 +0300

    new changes to config (org mode and key maps)

--- init.el
+++ init.el
@@ -114,9 +114,12 @@
   (setq doom-themes-enable-bold t
 	doom-themes-enable-italic t)
   ;; Often used themes
-  (load-theme 'doom-henna t)
+  ;; (load-theme 'doom-henna t)
   ;; (load-theme 'doom-nord-light t)
+  ;; (load-theme 'doom-nord t)
   ;; (load-theme 'doom-ayu-light t)
+  ;; (load-theme 'doom-city-lights t)
+  (load-theme 'doom-earl-grey t)
   (doom-themes-visual-bell-config))
 
 
@@ -159,7 +162,11 @@
   
   (rune/leader-keys
    "t"  '(:ignore t :which-key "toggles")
-   "tt" '(counsel-load-theme :which-key "choose theme")))
+   "tt" '(counsel-load-theme :which-key "choose theme")
+   "tf" '(find-file :which-key "find file")
+   "b"  '(:ignore t :which-key "buffers")
+   "bs" '(switch-to-buffer :which-key "switch to buffer")
+   "bk" '(kill-this-buffer :which-key "kill current buffer")))
 
 (defun rune/evil-hook ()
   (dolist (mode '(custom-mode
@@ -285,6 +292,82 @@
   :config
   (setq org-ellipsis " ▾"
 	org-hide-emphasis-markers t)
+
+  (setq org-agenda-start-with-log-mode t)
+  (setq org-log-done 'time)
+  (setq org-log-into-drawer t)
+
+  (setq org-agenda-files
+	'("~/Notes/tasks.org"
+	  "~/Notes/birthdays.org"))
+  (setq org-todo-keywords
+	'((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+	  (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
+
+  (setq org-tag-alist
+	'((:startgroup)
+	  ;; Put matually exclusive tags here
+	  (:endgroup)
+	  ("@errand" . ?E)
+	  ("@home" . ?H)
+	  ("@work" . ?W)
+	  ("agenda" . ?a)
+	  ("planning" . ?p)
+	  ("publish" . ?P)
+	  ("batch" . ?b)
+	  ("note" . ?n)
+	  ("idea" . ?i)
+	  ("thinking" . ?t)
+	  ("recurring" . ?r)))
+
+  ;; Configure custom agenda views
+  (setq org-agenda-custom-commands
+	'(("d" "Dashboard"
+	   ((agenda "" ((org-deadline-warning-days 7)))
+	    (todo "NEXT"
+		  ((org-agenda-overriding-header "Next Tasks")))
+	    (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
+
+	  ("n" "Next Tasks"
+	   ((todo "NEXT"
+		  ((org-agenda-overriding-header "Next Tasks")))))
+
+	  ("W" "Work Tasks" tags-todo "+work-email")
+
+	  ;; Low-effort next actions
+	  ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
+	   ((org-agenda-overriding-header "Low Effort Tasks")
+	    (org-agenda-max-todos 20)
+	    (org-agenda-files org-agenda-files)))
+
+	  ("w" "Workflow Status"
+	   ((todo "WAIT"
+		  ((org-agenda-overriding-header "Waiting on External")
+		   (org-agenda-files org-agenda-files)))
+	    (todo "REVIEW"
+	 	  ((org-agenda-overriding-header "In Review")
+	 	   (org-agenda-files org-agenda-files)))
+	    (todo "PLAN"
+	 	  ((org-agenda-overriding-header "In Planning")
+	 	   (org-agenda-todo-list-sublevels nil)
+	 	   (org-agenda-files org-agenda-files)))
+	    (todo "BACKLOG"
+	 	  ((org-agenda-overriding-header "Project Backlog")
+	 	   (org-agenda-todo-list-sublevels nil)
+	 	   (org-agenda-files org-agenda-files)))
+	    (todo "READY"
+	 	  ((org-agenda-overriding-header "Ready for Work")
+	 	   (org-agenda-files org-agenda-files)))
+	    (todo "ACTIVE"
+	 	  ((org-agenda-overriding-header "Active Projects")
+	 	   (org-agenda-files org-agenda-files)))
+	    (todo "COMPLETED"
+	 	  ((org-agenda-overriding-header "Completed Projects")
+	 	   (org-agenda-files org-agenda-files)))
+	    (todo "CANC"
+		  ((org-agenda-overriding-header "Cancelled Projects")
+		   (org-agenda-files org-agenda-files)))))))
+
   (efs/org-font-setup))
 
 (use-package org-bullets
@@ -294,7 +377,7 @@
   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
 ;; Open org mode link to other file in the same window
-(setf (cdr (assoc 'file org-link-frame-setup)) 'find-file)
+;; (setf (cdr (assoc 'file org-link-frame-setup)) 'find-file)
 
 (defun efs/org-mode-visual-fill ()
   (setq visual-fill-column-width 100
